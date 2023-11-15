987ceafd6364b85092e0830d91d87f7b6e7cec75
kcokros <warbandit.x@gmail.com>
2023-11-15 20:36:39 +0000

    Update TDL-TSDN2023.py

--- TDL-TSDN2023.py
+++ TDL-TSDN2023.py
@@ -21,7 +21,7 @@ def load_model():
     label_encoder_path = '.Model/label_encoder/label_encoder.pkl'
     loaded_tokenizer = DistilBertTokenizer.from_pretrained(tokenizer_path)
     loaded_model = DistilBertModel.from_pretrained(model_path, num_labels=2)
-    #loaded_label_encoder = joblib.load(label_encoder_path)
+    loaded_label_encoder = joblib.load(label_encoder_path)
     return loaded_model, loaded_tokenizer
 
 # Custom hashing function for anonymization
@@ -60,17 +60,52 @@ if uploaded_file is not None:
 # Anonymize data
 if st.button('Anonymize Data'):
     if uploaded_file is not None:
-        # Apply your anonymization logic here
-        # For example:
-        anonymized_df = df.applymap(hash_condition)
-        # Display anonymized data
-        st.dataframe(anonymized_df)
+        # Transform the data
+        melted_table = pd.melt(df, value_vars=df.columns, var_name='Column', value_name='Data')
+        
+        # Tokenize and encode the text data for DistilBert
+        encodings = loaded_tokenizer(list(melted_table['Data'].astype(str)), truncation=True, padding=True, max_length=64)
+        dataset = TensorDataset(torch.tensor(encodings['input_ids']), torch.tensor(encodings['attention_mask']))
+        loader = DataLoader(dataset, batch_size=8, shuffle=False)
+        
+        # Predict with DistilBert
+        loaded_model.eval()
+        all_preds = []
+        with torch.no_grad():
+            for batch in loader:
+                input_ids, attention_mask = batch
+                outputs = loaded_model(input_ids, attention_mask=attention_mask)
+                preds = torch.argmax(outputs.logits, dim=1).cpu().numpy()
+                all_preds.extend(preds)
+        
+        # Add DistilBert predicted labels to the DataFrame
+        melted_table['Sensitive_DB'] = loaded_label_encoder.inverse_transform(all_preds)
+        
+        # Predict with logistic regression
+        text_data = melted_table['Data'].astype(str).tolist()
+        logistic_preds = loaded_logistic_model.predict(text_data)
+        
+        # Add logistic regression predicted labels to the DataFrame
+        melted_table['Sensitive_LR'] = loaded_label_encoder.inverse_transform(logistic_preds)
 
-        # Convert DataFrame to CSV for download
-        csv = anonymized_df.to_csv(index=False)
-        st.download_button(
-            label="Download Anonymized Data",
-            data=csv,
-            file_name='anonymized_data.csv',
-            mime='text/csv',
-        )
+        # Apply SHA-256 hashing
+        melted_table.loc[melted_table['Predicted_Sensitive'] == 1, 'Data'] = \
+            melted_table.loc[melted_table['Predicted_Sensitive'] == 1, 'Data'].apply(
+                lambda x: hashlib.sha256(str(x).encode()).hexdigest())
+        # Apply custom MD5 hashing function
+        reverted_table = melted_table.applymap(hash_condition)
+
+        # Determine the file format for download based on the uploaded file
+        file_format = uploaded_file.name.split('.')[-1].lower()
+        if file_format == 'csv':
+            csv = anonymized_df.to_csv(index=False)
+            st.download_button(label="Download Anonymized Data as CSV", data=csv, file_name='anonymized_data.csv', mime='text/csv')
+        elif file_format in ['xlsx', 'xls']:
+            towrite = io.BytesIO()
+            anonymized_df.to_excel(towrite, index=False)
+            towrite.seek(0)
+            st.download_button(label=f"Download Anonymized Data as {file_format.upper()}", data=towrite, file_name=f'anonymized_data.{file_format}', mime='application/vnd.ms-excel')
+        else:
+            st.error("Unsupported file format. Please provide a CSV or Excel file.")
+
+This code first applies the anonymization logic, then checks the file extension of the uploaded file to determine the appropriate format for the download button. It handles CSV, XLSX, and XLS formats.
